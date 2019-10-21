8f97fff50432a5e16f274621f35b8f2a817272b2
Antonin Loubiere <antonin.loubiere@laposte.net>
2019-10-21 19:23:46 +0200

    Create splash screen activity
    Create the first version of the splash screen activity.

--- app/src/main/java/fr/pyjacpp/diakoluo/SplashScreenActivity.java
+++ app/src/main/java/fr/pyjacpp/diakoluo/SplashScreenActivity.java
@@ -1,14 +1,14 @@
 package fr.pyjacpp.diakoluo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
