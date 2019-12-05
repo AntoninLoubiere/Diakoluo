4125285a20505efddf77f7cd5df995193e2a14a3
Antonin Loubiere <antonin.loubiere@laposte.net>
2019-12-05 15:01:07 +0100

    Create skeleton of tests
    Create the skeleton of test.

---
+++ app/src/main/java/fr/pyjacpp/diakoluo/test_tests/TestSettingsActivity.java
@@ -0,0 +1,16 @@
+package fr.pyjacpp.diakoluo.test_tests;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.pyjacpp.diakoluo.R;
+
+public class TestSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_settings);
+    }
+}
