82a65e16662578aa9a7af518c6bd463b4bd4036e
Antonin Loubiere <antonin.loubiere@laposte.net>
2020-02-19 17:27:54 +0100

    Implement in list test, main information detail.
    Implement in list test, when screen is in landscape mode, the main information detail.

--- app/src/main/java/fr/pyjacpp/diakoluo/list_tests/ListTestActivity.java
+++ app/src/main/java/fr/pyjacpp/diakoluo/list_tests/ListTestActivity.java
@@ -11,17 +11,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import fr.pyjacpp.diakoluo.DiakoluoApplication;
 import fr.pyjacpp.diakoluo.R;
 import fr.pyjacpp.diakoluo.edit_test.EditTestActivity;
+import fr.pyjacpp.diakoluo.test_tests.TestSettingsActivity;
 import fr.pyjacpp.diakoluo.tests.Test;
 import fr.pyjacpp.diakoluo.view_test.MainInformationsViewTestFragment;
+import fr.pyjacpp.diakoluo.view_test.ViewTestActivity;
 
 public class ListTestActivity extends AppCompatActivity
         implements ListTestsFragment.OnFragmentInteractionListener,
         MainInformationsViewTestFragment.OnFragmentInteractionListener {
 
+    private boolean detailMainInformationTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_test);
+
+        detailMainInformationTest = findViewById(R.id.testMainInformationFragment) != null;
 
         FloatingActionButton addButton = findViewById(R.id.addFloatingButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +37,47 @@ public class ListTestActivity extends AppCompatActivity
                 startActivity(new Intent(ListTestActivity.this, EditTestActivity.class));
             }
         });
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        if (detailMainInformationTest) {
+            MainInformationsViewTestFragment mainInformationsViewTestFragment =(MainInformationsViewTestFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.testMainInformationFragment);
+
+            if (mainInformationsViewTestFragment != null) {
+                DiakoluoApplication.setCurrentTest(view.getContext(),
+                        DiakoluoApplication.getListTest(view.getContext()).get(position));
+
+                mainInformationsViewTestFragment.updateContent(this);
+            }
+        } else {
+            onSeeButtonClick(view, position);
+        }
+    }
+
+    @Override
+    public void onPlayButtonClick(View view, int position) {
+        Test currentTest = DiakoluoApplication.getListTest(view.getContext()).get(position);
+        if (currentTest.canBePlay()) {
+            DiakoluoApplication.setCurrentTest(view.getContext(),
+                    currentTest);
+
+            startActivity(new Intent(view.getContext(), TestSettingsActivity.class));
+        } // TODO warning
+    }
+
+    @Override
+    public void onSeeButtonClick(View view, int position) {
+        DiakoluoApplication.setCurrentTest(view.getContext(),
+                DiakoluoApplication.getListTest(view.getContext()).get(position));
+
+        startActivity(new Intent(view.getContext(), ViewTestActivity.class));
+    }
+
+    @Override
+    public void onEditMenuItemClick(View view, int position) {
+        DiakoluoApplication.setCurrentIndexEditTest(view.getContext(), position);
+        startActivity(new Intent(view.getContext(), EditTestActivity.class));
     }
 }
