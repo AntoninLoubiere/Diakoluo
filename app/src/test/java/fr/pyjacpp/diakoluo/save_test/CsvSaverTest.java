d3eaf92a6207a5d7effb60826ab53517fe6a56ef
Antonin Loubiere <antonin.loubiere@laposte.net>
2020-08-15 09:58:48 +0200

    Add a test for csvSaver
    Add a test to test separators in csvSavers.

---
+++ app/src/test/java/fr/pyjacpp/diakoluo/save_test/CsvSaverTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 LOUBIERE Antonin <https://www.github.com/AntoninLoubiere/>
+ *
+ * This file is part of Diakôluô project <https://www.github.com/AntoninLoubiere/Diakoluo/>.
+ *
+ *     Diakôluô is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Diakôluô is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     A copy of the license is available in the root folder of Diakôluô, under the
+ *     name of LICENSE.md. You could find it also at <https://www.gnu.org/licenses/gpl-3.0.html>.
+ */
+
+package fr.pyjacpp.diakoluo.save_test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CsvSaverTest {
+
+    @Test
+    public void separators() {
+        for (String separator : CsvSaver.SEPARATORS) {
+            assertEquals("Separators must have a length of 1 character !", 1,
+                    separator.length());
+        }
+    }
+
+}
