32fdc0038623f558d8f59d2e3006b7248515fa26
Antonin Loubiere <antonin.loubiere@laposte.net>
2020-08-15 09:36:18 +0200

    Add utils documentation and improve units tests
    Add documentation in Utils and improve units tests.

--- app/src/test/java/fr/pyjacpp/diakoluo/UtilsTest.java
+++ app/src/test/java/fr/pyjacpp/diakoluo/UtilsTest.java
@@ -12,6 +12,7 @@ public class UtilsTest {
         assertEquals("test", Utils.removeUselessSpaces("test   \n \t"));
         assertEquals("test", Utils.removeUselessSpaces("   \n \ttest"));
         assertEquals("test   \n \ttest", Utils.removeUselessSpaces("test   \n \ttest"));
+        assertEquals("test", Utils.removeUselessSpaces("test"));
     }
 
     @Test
@@ -21,5 +22,25 @@ public class UtilsTest {
         assertEquals(-100, Utils.map(-20, 0, 20, 0, 100));
         assertEquals(50, Utils.map(10, 0, 20, 0, 100));
         assertEquals(2, Utils.map(10, 0, 100, 0, 20));
+    }
+
+    @Test
+    public void removeUselessSpacesEnd() {
+        assertEquals("", Utils.removeUselessSpacesEnd(new StringBuilder("\n\r\t ")));
+        assertEquals("test", Utils.removeUselessSpacesEnd(new StringBuilder("test\n\r\t ")));
+        assertEquals("\ttest", Utils.removeUselessSpacesEnd(new StringBuilder("\ttest\n\r\t ")));
+        assertEquals("test", Utils.removeUselessSpacesEnd(new StringBuilder("test")));
+        assertEquals("test\n\r\t test", Utils.removeUselessSpacesEnd(new StringBuilder("test\n\r\t test")));
+    }
+
+    @Test
+    public void extremeDeceleratorInterpolator() {
+        Utils.ExtremeDeceleratorInterpolator extremeDeceleratorInterpolator = new Utils.ExtremeDeceleratorInterpolator();
+        float STEP = 0.01f;
+
+        for (float i = 0f; i <= 1f; i += STEP) {
+            float interpolation = extremeDeceleratorInterpolator.getInterpolation(i);
+            assertTrue(0 <= interpolation && interpolation <= 1);
+        }
     }
 }
